0aa576d2d8642e89995a9d23bb87c2f0d3a97b58
okm1011 <58724881+okm1011@users.noreply.github.com>
2024-04-17 18:18:52 +0900

    Update FROM_PLC_TO_AGV.py

--- AGV_related/FROM_PLC_TO_AGV.py
+++ AGV_related/FROM_PLC_TO_AGV.py
@@ -1,144 +1,24 @@
 import socket
 import struct
+import numpy as np
 import cv2
 import pickle
-import serial
+import time
 import threading
-from myagv import MyAgv
-import numpy as np
+import queue
 
-#dsad
-
-###########################################################################
-def initMotor() :	
-	print("init")
-
-def goForward() :
-	#print("goForward")
-	# print("F")
-	MA.go_ahead(1)
-
-def stopMotor() :
-	#print("stopMotor")
-	# print("S")
-	MA.stop()
-
-def goBackward() :
-	pass
-	#print("goBackward")
-	# print("B")
-	# MA.retreat(20, timeout=1)
-		
-def turnLeft() :
-	#print("turnLeft")
-	# print("L")
-	MA.counterclockwise_rotation(10)
-		
-def turnRight() :
-	#print("turnRight")
-	# print("R")
-	MA.clockwise_rotation(10)
-
-def exitMotor() :
-	print("exitMotor")
-  
-#####################################################################  
-
-initMotor()
-
-
-# bot = Rosmaster()   # add
-MA = MyAgv('/dev/ttyAMA2', 115200)
-
-speedFwd = 30 #speed for 0~90
-speedCurve = 30 #speed for 0~90
-
-
-# VIDSRC = 'v4l2src device=/dev/video0 ! video/x-raw,width=160,height=120,framerate=20/1 ! videoscale ! videoconvert ! jpegenc ! appsink'
-
-# cap=cv2.VideoCapture(VIDSRC, cv2.CAP_GSTREAMER)
-cap = cv2.VideoCapture(0)
-
-HOST = ''
+#HOST_RPI = '192.168.137.38'
+#PORT = 8089
+HOST_RPI = '172.30.1.12'
+# host = AGV랑 소켓 연결하기위한 HOST_RPI
 PORT = 8080
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('Socket created')
+# 두개 소켓 cam for 수신 from AGV 카메라 mot for 방향 송신 to AGV control
+client_cor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_cor.connect((HOST_RPI, PORT))
+print("Socket connected")
+time.sleep(10)
 
-server.bind((HOST, PORT))
-print('Socket bind complete')
-
-server.listen(10)
-print('Socket now listening')
-
-server_cam, addr = server.accept()
-server_mot, addr = server.accept()
-print('New Client.')
-
-flag_exit = False
-def mot_main() :
-
-	while True:
-		
-		rl_byte = server_mot.recv(1)
-		print(rl_byte)
-		rl = struct.unpack('!B', rl_byte)
-		#print(rl[0])
-	
-		right, left = (rl[0] & 2)>>1, rl[0] & 1
-		#print(right, left )
-		
-		if not right and not left:
-			goForward()
-		elif not right and left:
-			turnRight()
-		elif right and not left:
-			turnLeft()
-		else:
-			stopMotor()
-
-		if flag_exit: break
-
-motThread = threading.Thread(target=mot_main)
-motThread.start()
-
-try:
-
-	while True:	
-
-		cmd_byte = server_cam.recv(1)
-		cmd = struct.unpack('!B', cmd_byte)
-		# print(cmd[0])
-		if cmd[0]==12 :	
-		
-			# capture camera data
-			ret,frame=cap.read()
-			# print(frame.shape)
-
-			# data = frame
-
-			# Serialize frame
-			data = pickle.dumps(frame)
-			# print(len(data))
-
-			# send sensor + camera data
-			data_size = struct.pack("!L", len(data)) 
-			server_cam.sendall(data_size + data)
-			
-except KeyboardInterrupt:
-	pass
-except ConnectionResetError:
-	pass
-except BrokenPipeError:
-	pass
-except:
-	pass
-
-flag_exit = True
-motThread.join()
-	
-server_cam.close()
-server_mot.close()
-server.close()
-
-exitMotor()
+cmd = 1
+cmd_byte = struct.pack('!B', cmd)
+client_cor.sendall(cmd_byte)
